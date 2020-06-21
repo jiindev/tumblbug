2d924c3fb0c2edcbd3c59d149679311c16c28c5a
jiindev <jiindev@gmail.com>
2020-06-21 10:14:26 +0900

    backend 파일 간소화

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,6 @@
-import "./LoadEnv"; // Must be the first import
 import app from "@server";
 
 // Start the server
-const port = Number(process.env.PORT || 8000);
-app.listen(port, () => {
-  console.log("Express server started on port: " + port);
+app.listen(8000, () => {
+  console.log("Express server started on port: 8000");
 });
